a2a4213d20c4bdb17298def23e186806af5c45be
Ilya Vladimirskiy <bkmy43@googlemail.com>
2017-01-15 18:46:54 +0100

    Random run is working and smart run almost finalized

--- maze/maze.py
+++ maze/maze.py
@@ -45,19 +45,28 @@
 #     return false;
 # }
 import random
+import os
+import time
 
 class Maze:
-    walls = [[False]]
+    cur_x = 0
+    cur_y = 0
+    entrance = (0, 0)
+    max_steps = 50000
+    pause_between_steps = 0.05
 
     def __init__(self, height, width):
         self.height = height
         self.width = width
-        self.entrance = (0,0)
         self.exit = (height, width)
         self.walls = [[False for i in range(width)] for j in range(height)]
+        self.visited = [[False for i in range(width)] for j in range(height)]
+
+    def cur_position(self):
+        return (self.cur_y, self.cur_x)
 
     def display(self):
-        symbols = {"CUR_POSITION": u'\u2591\u2591', "EMPTY_CELL":'  ', "WALL": u'\u2588\u2588',
+        symbols = {"CUR_POSITION": u'ME', "VISITED": u'\u2591\u2591', "NOT_VISITED":'  ', "WALL": u'\u2588\u2588',
                    "H_BOUND": u'\u2501\u2501', "V_BOUND": u'\u2503',
                    "UP_LEFT": u'\u250F', "UP_RIGHT": u'\u2513', "BOTTOM_LEFT": u'\u2517', "BOTTOM_RIGHT": u'\u251B'}
 
@@ -78,13 +87,15 @@ class Maze:
                 str = ' '
 
             for j in range(self.width):
-                if (i,j) == self.entrance:
+                if (i,j) == self.cur_position():
                     str = str + symbols.get("CUR_POSITION")
                 else:
                     if self.walls[i][j]:
                         str = str + symbols.get("WALL")
+                    elif self.visited[i][j]:
+                        str = str + symbols.get("VISITED")
                     else:
-                        str = str + symbols.get("EMPTY_CELL")
+                        str = str + symbols.get("NOT_VISITED")
 
             if (i,j) != self.entrance and (i,j) != self.exit:
                 str = str + symbols.get("V_BOUND")
@@ -99,9 +110,11 @@ class Maze:
                 str = str + '  '
         print(str + symbols.get("BOTTOM_RIGHT"))
 
-    def randomize(self, walls_density=0.2):
+    def randomize(self, walls_density=0.25):
         self.entrance = self.get_random_position_on_maze_boundary()
         self.exit = self.get_random_position_on_maze_boundary()
+        self.cur_y = self.entrance[0]
+        self.cur_x = self.entrance[1]
 
         for i in range(self.height):
             for j in range(self.width):
@@ -116,27 +129,107 @@ class Maze:
 
         if wall == 'UP':
             return (0, random.choice(range(self.width)))
-
-        if wall == 'BOTTOM':
+        elif wall == 'BOTTOM':
             return (self.height-1, random.choice(range(self.width)))
-
-        if wall == 'LEFT':
+        elif wall == 'LEFT':
             return (random.choice(range(self.height)), 0)
-
-        if wall == 'RIGHT':
+        elif wall == 'RIGHT':
             return (random.choice(range(self.height)), self.width-1)
 
+    def move(self, direction):
+        if direction == 'UP':
+            if self.cur_y > 0 and not self.walls[self.cur_y-1][self.cur_x]:
+                self.cur_y = self.cur_y-1
+            else:
+                return False
+        elif direction == 'DOWN':
+            if self.cur_y < self.height - 1 and not self.walls[self.cur_y + 1][self.cur_x]:
+                self.cur_y = self.cur_y + 1
+            else:
+                return False
+        elif direction == 'LEFT':
+            if self.cur_x > 0 and not self.walls[self.cur_y][self.cur_x - 1]:
+                self.cur_x = self.cur_x - 1
+            else:
+                return False
+        elif direction == 'RIGHT':
+            if self.cur_x < self.width - 1 and not self.walls[self.cur_y][self.cur_x + 1]:
+                self.cur_x = self.cur_x + 1
+            else:
+                return False
 
-m1 = Maze(10,10)
-m1.randomize(0.2)
+        self.visited[self.cur_y][self.cur_x] = True
+        return True
+
+    def chaotic_run(self):
+        for i in range(self.max_steps):
+            time.sleep(self.pause_between_steps)
+            os.system('clear')
+            m1.move(random.choice(('UP', 'DOWN', 'LEFT', 'RIGHT')))
+            m1.display()
+            if self.cur_position() == self.exit:
+                print("Sucessfully reached exit. {} steps were needed".format(i + 1))
+                return True
+            print("This is CHAOTIC RUN\nTrying to get out of this maze... {} steps done".format(i+1))
+            print(self.get_best_move())
+
+        print("Maximum of {} steps reached, exit not found...".format(i+1))
+        return False
+
+
+    def smart_run(self):
+        for i in range(self.max_steps):
+            time.sleep(self.pause_between_steps)
+            os.system('clear')
+            m1.move(self.get_best_move())
+            m1.display()
+            if self.cur_position() == self.exit:
+                print("Sucessfully reached exit. {} steps were needed".format(i + 1))
+                return True
+            print("This is SMART RUN\nTrying to get out of this maze... {} steps done".format(i + 1))
+
+        print("Maximum of {} steps reached, exit not found...".format(i + 1))
+        return False
+
+    def get_best_move(self):
+        options = {'UP':0, 'DOWN':0, 'LEFT':0, 'RIGHT':0}
+
+        for option in options.keys():
+            if option == 'UP':
+                if self.cur_y > 0 and not self.walls[self.cur_y - 1][self.cur_x]:
+                    options[option] = self.get_distance(self.exit, (self.cur_y-1, self.cur_x))
+                else:
+                    options[option] = self.width + self.height
+            elif option == 'DOWN':
+                if self.cur_y < self.height - 1 and not self.walls[self.cur_y + 1][self.cur_x] and not self.visited[self.cur_y + 1][self.cur_x]:
+                    options[option] = self.get_distance(self.exit, (self.cur_y + 1, self.cur_x))
+                else:
+                    options[option] = self.width + self.height
+            elif option == 'LEFT':
+                if self.cur_x > 0 and not self.walls[self.cur_y][self.cur_x - 1]  and not self.visited[self.cur_y][self.cur_x - 1]:
+                    options[option] = self.get_distance(self.exit, (self.cur_y, self.cur_x-1))
+                else:
+                    options[option] = self.width + self.height
+            elif option == 'RIGHT':
+                if self.cur_x < self.width - 1 and not self.walls[self.cur_y][self.cur_x + 1] and not self.visited[self.cur_y][self.cur_x + 1]:
+                    options[option] = self.get_distance(self.exit, (self.cur_y, self.cur_x - 1))
+                else:
+                    options[option] = self.width + self.height
+
+        return min(options, key=options.get)
+
+    def get_distance(self,point1, point2):
+        return abs(point1[0]-point2[0]) + abs(point1[1]-point2[1])
+
+os.environ['TERM']='xterm'
+
+m1 = Maze(40,40)
+m1.randomize(0.25)
 m1.display()
 
-
-m1 = Maze(15,15)
-m1.randomize(0.3)
-m1.display()
+m1.smart_run()
 
 
-m1 = Maze(20,20)
-m1.randomize(0.4)
-m1.display()
+
+
+
